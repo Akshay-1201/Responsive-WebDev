f54cb14367c4a3ca134f9739348d18da5823c093
Akshay-1201 <akshaykute12@gmail.com>
2023-08-22 17:59:23 +0530

    animation added

--- Project 1/script.js
+++ Project 1/script.js
@@ -2,9 +2,34 @@
 //     el: document.querySelector('#main'),
 //     smooth: true
 // });
+
 function circleMouseFollower(){
     window.addEventListener("mousemove", function(dets){
         document.querySelector("#mincircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
     })
 }
 circleMouseFollower();
+function firstPageAnim(){
+    let tl = gsap.timeline();
+    tl.from(".nav", {
+        y: '10',
+        opacity:0,
+        duration: 1,
+        ease: Expo
+    })
+    .to(".boundingelem", {
+        y:0,
+        ease:Expo.easeInout,
+        duration:1,
+        stagger:.2,
+        delay:-1
+    })
+    .from(".hero-footer", {
+        y: '-10',
+        opacity:0,
+        duration: 1.5,
+        ease: Expo.easeInout,
+        delay:-1
+    })
+}
+firstPageAnim();
