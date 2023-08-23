dd69a46a200a5836e173c39adaf97080c5ac400e
Akshay-1201 <akshaykute12@gmail.com>
2023-08-23 22:17:38 +0530

    circle flatten animation add

--- Project 1/script.js
+++ Project 1/script.js
@@ -1,11 +1,11 @@
 // const scroll = new LocomotiveScroll({
-//     el: document.querySelector('#main'),
+//     el:document.querySelector('#main'),
 //     smooth: true
 // });
-
-function circleMouseFollower(){
+let timeout ;
+function circleMouseFollower(xscale,yscale){
     window.addEventListener("mousemove", function(dets){
-        document.querySelector("#mincircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
+        document.querySelector("#mincircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(${xscale}, ${yscale})`;
     })
 }
 circleMouseFollower();
@@ -33,3 +33,28 @@ function firstPageAnim(){
     })
 }
 firstPageAnim();
+ function flatCircle(){
+    let xscale = 1;
+    let yscale = 1;
+
+    let xprev = 0;
+    let yprev = 0;
+
+    window.addEventListener("mousemove", function(dets){
+        clearTimeout(timeout);
+
+        xscale = gsap.utils.clamp(.8,1.2, dets.clientX - xprev);
+        yscale = gsap.utils.clamp(.8,1.2, dets.clientY - yprev);
+        
+        yprev = dets.clientY;
+        xprev = dets.clientX;
+
+        circleMouseFollower(xscale,yscale);
+
+        timeout = setTimeout(() => {
+            document.querySelector("#mincircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(1,1)`;
+        }, 100);
+        
+    })
+ }
+ flatCircle();
